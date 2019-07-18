0525bff5682efa21f4f572645d5017d71357b2a9
commaeng <commaeng09@gmail.com>
2019-07-18 22:24:11 +0900

    12.game

--- js/12.game.js
+++ js/12.game.js
@@ -26,3 +26,38 @@ $("#btReset").click(function(){
 	$("#btInit").show();
 	$("#stage").empty();
 });
+
+$("#btStart").click(function(){
+	var ranking = [];
+	$(".pic").each(function(){
+		var speed = Math.ceil(Math.random() * 3000 + 3000);
+		$(this).animate({"left":"90%"}, speed, function(){
+			//console.log(	$(this).index()	);
+			ranking.push($(this).index());
+			if(ranking.length == $("#cnt").val()) {
+				var html = ''
+				for(var i=0; i<ranking.length; i++) {
+					html += '<h3>'+(ranking[i]+1)+'번</h3>';
+				}
+				$(".modal-body").find("p").html(html);
+				$("#modal").modal();
+			}	
+		});
+	});
+
+	//console.log(speed);
+	/*
+	for(var i=0; i<$(".pic").length; i++) {
+		speed = Math.ceil(Math.random() * 3000 + 3000);
+		console.log(speed);
+		$(".pic").eq(i).animate({"left": "90%"}, speed);
+	}
+	*/
+});
+
+/* 
+Math.ceil  => 올림
+Math.floor => 버림
+Math.round => 반올림
+Math.abs   => 절대값
+*/
