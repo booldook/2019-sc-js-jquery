dd616e1a74f8a82ce88f13359b4cadfea578e6a1
commaeng <commaeng09@gmail.com>
2019-07-30 21:22:51 +0900

    16.slide.js

---
+++ js/16.slide.js
@@ -0,0 +1,51 @@
+/*
+1. setInterval(fn, delay(ms)) : delay 시간마다 fn을 실행해주는 함수
+setInterval(function(){
+
+}, 3000);
+
+// 정해진 시간마다 페이지 새로고침 예제
+setInterval(function(){
+	location.reload();
+}, 5000);
+
+for(var i=0; i<5; i++) {
+	$(".slide").eq(i).css({"left":(i*100)+"%"});
+}
+*/
+
+//IIFE => 즉시실행함수표현식(Immediatly Invoke Function Expression)
+(function () {
+	var now = 1;
+	var speed = 100;
+	var gap = 1000;
+	$(".slides > .slide").each(function(i){
+		$(this).css({"left": (i*100)+"%"});
+	});
+	setInterval(function(){
+		$(".slides").stop().animate({"left": -(now*100)+"%"}, speed, function(){
+			if(now == $(".slides > .slide").length - 1) now = 0;
+			else now++;
+		});
+	}, gap);
+})();
+
+(function(){
+	var now = 1;
+	var speed = 500;
+	var gap = 3000;
+	$(".slides2").append($(".slides2 > .slide").eq(0).clone());
+	$(".slides2 > .slide").each(function(i){
+		$(this).css({"left": (i*100)+"%"});
+	});
+	setInterval(function(){
+		$(".slides2").stop().animate({"left": -(now*100)+"%"}, speed, function(){
+			if(now == $(".slides2 > .slide").length - 1) {
+				$(".slides2").css({"left": 0});
+				now = 1;
+			}
+			else now++;
+		});
+	}, gap);
+})();
+
